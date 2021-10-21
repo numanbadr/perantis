ef07dc2e7768358809364591464b55d7a60149ac
Nu'man Badr <47119026+numanbadr@users.noreply.github.com>
2021-10-22 00:04:06 +0800

    Add files via upload

---
+++ assignment4/NumanModule.py
@@ -0,0 +1,12 @@
+def greeting(name):
+    print(f'Hello {name}')
+
+    
+def goodbye(name):
+    print(f'Bye {name}')
+    
+person1 = {
+    'name' : 'John',
+    'age' : 36,
+    'country' : 'Norway'
+}
